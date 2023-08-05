ad91320f056118f8d7a7d5e4e5dca967942c3168
ciaranbyrne15 <byrneciaran2003@gmail.com>
2023-08-06 00:20:55 +0100

    easier UI for the usernto select measurement

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -6,9 +6,35 @@ namespace ConsoleAppProject.App01
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Ciaran Byrne version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+
+        public void Run()
+        {
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+        private void InputMiles()
+        {
+            Console.Write("Please enter the number of miles > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+
+        }
+
+        private void CalculateFeet()
+        {
+            feet = miles * 5280;
+        }
+
+        private void OutputFeet()
+        {
+            Console.WriteLine(miles + " miles is " + feet + "feet");
+        }
     }
 }
